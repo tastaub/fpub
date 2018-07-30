a8addac7c8460cbf6101b89fc88d8e4513d6e331
Thomas Staub <tastaub@gmail.com>
2018-07-30 18:35:32 -0400

    Initial Express Server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://thomas:thomas123@ds159641.mlab.com:59641/dev-data"
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const post = require("./routes/api/post");
+
+const app = express();
+
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/post", post);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
