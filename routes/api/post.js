22d0171249a4432fd08a95e1bfdf15f73b0ef593
Thomas Staub <tastaub@gmail.com>
2018-08-18 12:55:27 -0400

    Building for production

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,127 +1,71 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const passport = require("passport");
 
-// Load profile model
-const Profile = require("../../models/Profile");
-// Load user model
-const User = require("../../models/User");
+const Beer = require("../../models/Beer");
+const Food = require("../../models/Food");
+const Events = require("../../models/Events");
 
-// @route GET api/profile/test
-// @desc Test profile route
+// @route GET api/post/getbeer
+// @desc Get all beers in db
 // @access Public
-router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
-
-// @route GET api/profile
-// @desc Get current users profile
-// @access Private
-router.get(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const errors = {};
-    Profile.findOne({ user: req.user.id })
-      .populate("user", ["name", "avatar"])
-      .then(profile => {
-        if (!profile) {
-          errors.noprofile = "There is no profile for this user";
-          return res.status(404).json(errors);
-        }
-      })
-      .catch(err => res.status(404).json(err));
-  }
-);
-
-// @route GET api/profile/handle/:handle
-// @desc  Get profile by handle
-// @access Public
-router.get("/handle/:handle", (req, res) => {
-  const errors = {};
-
-  Profile.findOne({ handle: req.params.handle })
-    .populate("user", ["name", "avatar"])
-    .then(profile => {
-      if (!profile) {
-        errors.noprofile = "There is no profile for the user";
-        res.status(404).json(errors);
-      }
-      res.json(profile);
-    })
-    .catch(err => res.status(404).json(err));
-});
-
-// @route GET api/profile/user/:user_id
-// @desc  Get profile by user ID
-// @access Public
-router.get("/user/:user_id", (req, res) => {
-  const errors = {};
-
-  Profile.findOne({ handle: req.params.user_id })
-    .populate("user", ["name", "avatar"])
-    .then(profile => {
-      if (!profile) {
-        errors.noprofile = "There is no profile for the user";
-        res.status(404).json(errors);
-      }
-      res.json(profile);
-    })
-    .catch(err => res.status(404).json(err));
-});
-
-// @route POST api/profile
-// @desc  Create or edit user profile
-// @access Private
-router.post(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    // Get fields
-    const profileFields = {};
-    profileFields.user = req.user.id;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = req.body.company;
-    if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bio) profileFields.bio = req.body.bio;
-    if (req.body.status) profileFields.status = req.body.status;
-    if (req.body.githubusername)
-      profileFields.githubusername = req.body.githubusername;
-
-    // Skills - Split into array
-    if (typeof req.body.skills !== "undefined") {
-      profileFields.skills = req.body.skills.split(",");
+router.get("/beer", (req, res) => {
+  Beer.find({}, (err, response) => {
+    if (err) {
+      res.json(err);
     }
-    // Social
-    profileFields.socical = {};
-    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    res.json(response);
+  });
+});
 
-    Profile.findOne({ user: req.user.id }).then(profile => {
-      if (profile) {
-        // Update
-        Profile.findOneAndUpdate(
-          { user: req.user.id },
-          { $set: profileFields },
-          { new: true }
-        ).then(profile => res.json(profile));
-      } else {
-        // Create
-        // Check if handle exists
-        Profile.findOne({ handle: profileFields.handle }).then(profile => {
-          if (profile) {
-            errors.handle = "That handle already exists";
-            res.status(404).json(errors);
-          }
+// @route GET api/post/getbeer
+// @desc Get all beers in db
+// @access Public
+router.post("/beer", (req, res) => {
+  let newBeer = req.body;
+  console.log(newBeer);
+  Beer.create(newBeer).then(data => res.json(data));
+});
 
-          new Profile(profileFields).save().then(profile => res.json(profile));
-        });
-      }
-    });
-  }
-);
+// @route GET api/post/getfood
+// @desc Get all food in db
+// @access Public
+router.get("/food", (req, res) => {
+  Food.find({}, (err, response) => {
+    if (err) {
+      res.json(err);
+    }
+    res.json(response);
+  });
+});
+
+// @route GET api/post/getbeer
+// @desc Get all beers in db
+// @access Public
+router.post("/food", (req, res) => {
+  let newFood = req.body;
+  console.log(newFood);
+  Food.create(newFood).then(data => res.json(data));
+});
+
+// @route GET api/post/events
+// @desc Get all events in db
+// @access Public
+router.get("/events", (req, res) => {
+  Events.find({}, (err, response) => {
+    if (err) {
+      res.json(err);
+    }
+    res.json(response);
+  });
+});
+
+// @route POST api/post/events
+// @desc Post new events
+// @access Public
+router.post("/events", (req, res) => {
+  let newEvents = req.body;
+  console.log(newEvents);
+  Events.create(newEvents).then(data => res.json(data));
+});
 
 module.exports = router;
